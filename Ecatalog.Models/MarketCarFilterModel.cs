cad33b58133b81117db1f737f017eb772fb83c60
warakornaac <warakorn.pra@aac.co.th>
2026-06-09 10:12:51 +0700

    [controller] add function get master marker

---
+++ Ecatalog.Models/MarketCarFilterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecatalog.Models
+{
+    public class MarketCarFilterModel
+    {
+        public int statusCode { get; set; }
+        public string errorMessage { get; set; }
+        public List<ResultList> result { get; set; }
+    }
+    public class ResultList
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Ecatalog/Controllers/MasterController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Ecatalog.Models;
+using Ecatalog.Library;
+
+namespace Ecatalog.Controllers
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public async Task<ActionResult> GetMarketCar(string moduleId) {
+            try {
+                var result = await Utils.CallApiAsyncMemory<
+                        MarketCarFilterModel>(
+                        "Ecatalog/GetMarketCar",
+                        "GET",
+                        new {
+                            moduleId = moduleId
+                        },
+                        false,
+                        10);
+
+                if (result.IsSuccess &&
+                    result.Data != null &&
+                    result.Data.result != null &&
+                    result.Data.result.Count > 0) {
+                    var user = result.Data.result.FirstOrDefault();
+                }
+
+                return Json(new {
+                    IsSuccess = result.IsSuccess,
+                    IsFromCache = result.IsFromCache,
+                    Data = result.Data?.result
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex) {
+                return Json(new {
+                    IsSuccess = false,
+                    Message = ex.Message
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+        }
+        public async Task<ActionResult> GetModelRange(string marketSegmentId, string segmentId, string makerId) {
+            try {
+                var result = await Utils.CallApiAsyncMemory<ModelRangeFilterModel>(
+                    "Ecatalog/GetModelRange",
+                    "GET",
+                    new {
+                        marketSegmentId,
+                        segmentId,
+                        makerId
+                    },
+                    true,
+                    10);
+
+                return Json(new {
+                    IsSuccess = result.IsSuccess,
+                    IsFromCache = result.IsFromCache,
+                    Data = result.Data?.result
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex) {
+                return Json(new {
+                    IsSuccess = false,
+                    Message = ex.Message
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
