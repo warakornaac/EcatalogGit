fe3617a39e4ad8d42fffb3ff174f3e455e035077
warakornaac <warakorn.pra@aac.co.th>
2026-07-10 09:14:06 +0700

    [MeiliSearch] add service MeiliSearch

---
+++ Ecatalog.Models/SearchDictionaryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecatalog.Models
+{
+    public class SearchDictionaryModel
+    {
+        public int Id { get; set; }
+
+        public string Keyword { get; set; }
+
+        public string Normalize { get; set; }
+
+        public string SearchType { get; set; }
+
+        public string SourceTable { get; set; }
+
+        public int Priority { get; set; }
+
+        public string LanguageCode { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime InsertedDate { get; set; }
+    }
+}
